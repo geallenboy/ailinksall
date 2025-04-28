3efdd7e8f2c0755c120e78680e18c76f10caf786
gejialun <gejialun88@163.com>
2025-04-28 15:56:49 +0800

    aichat0.1

--- src/hooks/chat/index.ts
+++ src/hooks/chat/index.ts
@@ -6,11 +6,9 @@ export * from "./use-image-attachment";
 export * from "./use-llm-test";
 export * from "./use-mdx";
 export * from "./use-model-list";
-// export * from "./use-preference-hooks";
 export * from "./use-prompts";
 export * from "./use-record-voice";
 export * from "./use-scroll-to-bottom";
 export * from "./use-tools";
 export * from "./usse-text-selection";
-// export * from "./use-session-hooks"
 
